fd1f3ed91532bc7377bac1ad9acf778ad19e7751
anates <developer022@quantentunnel.de>
2014-09-22 11:22:26 +0200

    Ein paar Funktionen hinzugefuegt, fuer weitere Aenderungen siehe Functions.py

--- nested/Functions.py
+++ nested/Functions.py
@@ -5,6 +5,8 @@ Created on 19.09.2014
 '''
 
 import hl7
+import psycopg2
+from numpy.distutils.mingw32ccompiler import _TABLE
 #Zieht sich die Namen des Patienten aus der HL7-Nachricht
 def getNames(parsed_message):
     if(isinstance(parsed_message, hl7.Message) == False):
@@ -13,7 +15,7 @@ def getNames(parsed_message):
         return (str(parsed_message.segment("PID")[5][0][1][0]), str(parsed_message.segment("PID")[5][0][0][0]))
 
 
-#Gibt sämtliche vorgenommenen Untersuchungen (ohne weitere Infos) aus
+#Gibt saemtliche vorgenommenen Untersuchungen (ohne weitere Infos) aus
 def getObservablen(parsed_message):
     Observables = {}
     if(isinstance(parsed_message, hl7.Message) == False):
@@ -35,4 +37,22 @@ def getWhatHappened(parsed_message):
             if elem >= 3:
                 Observables[elem-3] = parsed_message[elem-1][0][0]
     return Observables
+
+#Schaut, ob Datenbank ueberhaupt vorhanden ist
+def checkIfDatabaseExists(_database, _user, _host, _password):
+    con = None
+    con = psycopg2.connect(database=_database, user=_user, host=_host, password=_password)
+    cur = con.cursor()
+    cur.execute("SELECT exists(SELECT 1 from pg_catalog.pg_database where datname = %s)", (_database,))
+    ver = cur.fetchone()[0]
+    return ver
+
+#Schaut, ob Tabelle in Datenbank vorhanden ist
+def checkIfTableExists(_database, _user, _host, _password, _table):
+    con = None
+    con = psycopg2.connect(database=_database, user=_user, host=_host, password=_password)
+    cur = con.cursor()
+    cur.execute("select exists(select * from information_schema.tables where table_name=%s)", (_table,))
+    ver = cur.fetchone()[0]
+    return ver
                 
--- nested/test.py
+++ nested/test.py
@@ -5,10 +5,8 @@ Created on 16.09.2014
 '''
 import hl7
 import sys
-from Functions import getNames
-from Functions import getObservablen
-
-
+import psycopg2
+from Functions import *
 
 message = ''
 # message2 = 'MSH|^~\&|GHH LAB|ELAB-3|GHH OE|BLDG4|200202150930||ORU^R01|CNTRL-3456|P|2.4\r'
@@ -37,4 +35,52 @@ Untersuchungen = getObservablen(h)
 print PatientBookfull.keys()
 print Untersuchungen
 print PatientBookSimple
+
+print "Existiert die Datenbank?"
+print checkIfDatabaseExists("testdb", "test", "localhost", "abcd")
+print "Existiert die Tabelle?"
+print checkIfTableExists("testdb", "test", "localhost", "abcd", "cars")
+#===============================================================================
+# cars = (
+#     (1, 'Audi', 52642),
+#     (2, 'Mercedes', 57127),
+#     (3, 'Skoda', 9000),
+#     (4, 'Volvo', 29000),
+#     (5, 'Bentley', 350000),
+#     (6, 'Citroen', 21000),
+#     (7, 'Hummer', 41400),
+#     (8, 'Volkswagen', 21600)
+# )
+#   
+# con = None
+#   
+# try:
+#        
+#     con = psycopg2.connect(database='testdb', user='test', password="abcd")  
+#      
+#     cur = con.cursor()    
+#     cur.execute("SELECT * FROM cars")
+#  
+#     rows = cur.fetchall()
+#  
+#     for row in rows:
+#         print row
+#       
+#   
+# except psycopg2.DatabaseError, e:
+#       
+#     if con:
+#         con.rollback()
+#       
+#     print 'Error %s' % e    
+#     sys.exit(1)
+#       
+#       
+# finally:
+#       
+#     if con:
+#         con.close()
+#===============================================================================
+        
+
 
