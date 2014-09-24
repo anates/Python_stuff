a469244af4c37b662fe916a6c5ea9159a5ca355d
anates <developer022@quantentunnel.de>
2014-09-24 17:17:30 +0200

    Konvertierung von JSON in HL7 und zurück eingebaut

--- nested/Functions.py
+++ nested/Functions.py
@@ -8,6 +8,8 @@ Created on 19.09.2014
 import hl7
 import psycopg2
 import json
+from hl7 import ishl7
+from hl7apy.parser import parse_message
 
 def getNames(parsed_message):
     '''Zieht sich die Namen des Patienten aus der HL7-Nachricht (funktioniert)'''
@@ -101,13 +103,63 @@ def createNewTable(_database, _user, _host, _password, _tablename, _patientdata)
         return True
     
 def createLUTTable(PID, position):
-    '''Gibt die Funktion des Segments an Position 'position' des Elements 'PID' zurück'''
+    '''Gibt die Funktion des Segments an Position 'position' des Elements 'PID' zurück, noch nicht implementiert'''
     
+def checkHL7Message(_HL7_message):
+    '''Überprüft, ob die eingehende Nachricht eine HL7-Nachricht ist'''
+    return ishl7(_HL7_message)
+
+def createMSHMessage(_JSON_message):
+    '''Erstellt aus der JSON-Nachricht die erste Zeile für die HL7-Nachricht'''
+    _data = json.loads(_JSON_message)
+    _MSH_message = _data[0][0][0]
+    _primary_separator = _data[0][1][0]
+    _secondary_separator = {}
+    for i, elem in enumerate(_data[0][2][0]):
+        _secondary_separator[i] = elem
+    for i in xrange(len(_data[0])-2):
+        if(type(_data[0][i+2][0]) == unicode):
+            _MSH_message += _primary_separator + _data[0][i+2][0]
+            if(_data[0][i+2][0] == ''):
+                _MSH_message += _primary_separator
+        else:
+            _tmp_string = ""
+            for j in xrange(len(_data[0][i+2][0])):
+                _tmp_string += _data[0][i+2][0][j][0]
+                _tmp_string += _secondary_separator[0]
+            _tmp_string = _tmp_string[:-1]
+            _MSH_message += _tmp_string
+    return _MSH_message, _primary_separator, _secondary_separator
                 
 def createJSONObject(_parsed_message):
     '''Wandelt die HL7-Nachricht in ein JSON-Object um'''
     return json.dumps(_parsed_message)                    
 
+def createHL7String(_JSON_message, _primary_separator, _secondary_separator, number):
+    '''Erstellt aus der JSON-Nachricht HL7-Strings, und gibt diese zurück. Sie werden nicht geparst, das wird später übernommen'''
+    _HL7_message = _JSON_message[number][0][0]
+    for i in xrange(len(_JSON_message[number])-1):
+        if(type(_JSON_message[number][i+1][0]) == unicode):
+            _HL7_message += _primary_separator + _JSON_message[number][i+1][0]
+        else:
+            _tmp_string = _primary_separator
+            for j in xrange(len(_JSON_message[number][i+1][0])):
+                _tmp_string += _JSON_message[number][i+1][0][j][0]
+                _tmp_string += _secondary_separator[0]
+            _tmp_string = _tmp_string[:-1]
+            _HL7_message += _tmp_string
+    return _HL7_message
+
+def createHL7Message(_JSON_message):
+    '''Erstellt aus der JSON-Nachricht mithilfe der Funktion createHL7String eine komplette HL7-Nachricht, frisst aber nur ungeparste JSON-Nachrichten'''
+    _MSH_message, _primary_separator, _secondary_separator = createMSHMessage(_JSON_message)
+    _parsed_message = json.loads(_JSON_message)
+    _HL7_message = _MSH_message + '\r'
+    #print _HL7_message
+    for i in xrange(len(_parsed_message)-1):
+        _HL7_message += createHL7String(_parsed_message, _primary_separator, _secondary_separator, i + 1) + '\r\n'
+    _HL7_message = _HL7_message[:]
+    return _HL7_message
     
     
     
--- nested/test.py
+++ nested/test.py
@@ -8,9 +8,7 @@ import hl7
 import sys
 import psycopg2
 from Functions import *
-from __builtin__ import str
 import json
-
 
 
 _filename_in = ""
@@ -21,6 +19,8 @@ _message_array = []
 
 _patient_data = {}
 
+_HL7_to_JSON = True
+
 if(len(sys.argv) != 3):
     _use_stdin = True
 else:
@@ -29,7 +29,7 @@ else:
     _filename_out = sys.argv[2]
     
 
-    
+   
 
 
 if not _use_stdin:
@@ -40,89 +40,39 @@ else:
     for line in sys.stdin:
         _message += line[:-1]
 
-#parse message
-_h = hl7.parse(_message)
-
-#print isinstance(h, hl7.Message)
-#print type(h)
-#-------------------------------------------------------- print h.segment("PID")
-#--------------------------------------------------- print len(h.segment("PID"))
-#------------------------------------------------ print len(h.segment("PID")[5])
-#--------------------------------------------- print len(h.segment("PID")[5][0])
-#------------------------------------------ print len(h.segment("PID")[5][0][0])
-#--------------------------------------- print len(h.segment("PID")[5][0][0][0])
-print getNames(_h)
-PatientBookfull = {getNames(_h):_h}
-PatientBookfull[getNames(_h)] = _h
-PatientBookSimple = {getNames(_h):getObservablen(_h)}
-Untersuchungen = getObservablen(_h)
-
-print _h[0]
-
-for i, elem in enumerate(Untersuchungen):
-    _patient_data[elem] = _h[i]
-
-_JSON = createJSONObject(_h)
-if(_use_stdin):
-    print _JSON
+if(checkHL7Message(_message) or (_filename_in[-3:] == "hl7")):
+    _HL7_to_JSON = True
 else:
-    with open(_filename_out, "w") as text_file:
-        text_file.write(_JSON)
-#===============================================================================
-# print PatientBookfull.keys()
-# print Untersuchungen
-# print PatientBookSimple
-# 
-# print "Existiert die Datenbank?"
-# print checkIfDatabaseExists("testdb", "test", "localhost", "abcd")
-# print "Existiert die Tabelle?"
-# print checkIfTableExists("testdb", "test", "localhost", "abcd", "cars")
-# delTable("testdb", "test", "localhost", "abcd", "cars")
-#===============================================================================
+    _HL7_to_JSON = False
 
-Patient = {'name':'Mustermann', 'surname':'Max', 'birthdate': '01011990', 'PID': 99999999}
+if(_HL7_to_JSON):
+    _h = hl7.parse(_message)
+    print getNames(_h)
+    PatientBookfull = {getNames(_h):_h}
+    PatientBookfull[getNames(_h)] = _h
+    PatientBookSimple = {getNames(_h):getObservablen(_h)}
+    Untersuchungen = getObservablen(_h)
+    
+    for i, elem in enumerate(Untersuchungen):
+        _patient_data[elem] = _h[i]
+    
+    _JSON = createJSONObject(_h)
+    if(_use_stdin):
+        print _JSON
+    else:
+        with open(_filename_out, "w") as text_file:
+            text_file.write(_JSON)
+else:
+    _MSH_message, _primary_separator, _secondary_separator = createMSHMessage(_message)
+    _HL7_message = createHL7Message(_message)
+    if(_use_stdin):
+        print _HL7_message
+    else:
+        outfile = open(_filename_out, "w")
+        outfile.write(_HL7_message)
+        outfile.close()
 
-#createNewTable("testdb", "test", "localhost", "abcd", "ErsterPatient", Patient)
+    
 
-cars = (
-    ('Audi', 1, 52642),
-    ('Mercedes', 2, 57127),
-    ('Skoda', 3, 9000),
-    ('Volvo', 4, 29000),
-    ('Bentley', 5, 350000),
-    ('Citroen', 6, 21000),
-    ('Hummer', 7, 41400),
-    ('Volkswagen', 8, 21600)
-)
-    
-con = None
-    
-try:
-         
-    con = psycopg2.connect(database='testdb', user='test', password="abcd")  
-       
-    cur = con.cursor()    
-    cur.execute("DROP TABLE IF EXISTS cars")
-    cur.execute("CREATE TABLE cars(id INT PRIMARY KEY, name TEXT, price INT)")
-    query = "INSERT INTO cars (name, id, price) VALUES (%s, %s, %s)"
-    cur.executemany(query, cars)
-        
-    con.commit()
-        
-    
-except psycopg2.DatabaseError, e:
-        
-    if con:
-        con.rollback()
-        
-    print 'Error %s' % e    
-    sys.exit(1)
-        
-        
-finally:
-        
-    if con:
-        con.close()
-         
 
 
